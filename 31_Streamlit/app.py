17475f991db3b8f59257c360bce2a5743141a92d
Bryson Xue <002831@i-pass.com.tw>
2021-01-28 18:46:44 +0800

    pandas multi index

--- 31_Streamlit/app.py
+++ 31_Streamlit/app.py
@@ -17,19 +17,52 @@ st.date_input('start date')
 st.date_input('end date')
 
 # 加入 pandas 資料表格
-st.write(pd.DataFrame({
-    'first column': [1, 2, 3, 4],
-    'second column': [10, 20, 30, 40]
-}))
+# st.write(pd.DataFrame({
+#     'first column': [1, 2, 3, 4],
+#     'second column': [10, 20, 30, 40]
+# }))
 
+# multi_index = pd.MultiIndex.from_product([
+#     ['city_1', 'city_2'],
+#     ['store_1', 'store_2', 'store_3'],
+# ])
+
+# df = pd.DataFrame(columns=['apples', 'oranges'], index=multi_index)
+# df.index.set_names(['city', 'store'], inplace=True)
+
+# # Now let's make some selectboxes for drilling up/down
+# levels = [
+#     st.selectbox('Level 1', ['All'] +
+#                  [i for i in df.index.get_level_values(0).unique()]),
+#     st.selectbox('Level 2', ['All'] +
+#                  [i for i in df.index.get_level_values(1).unique()])
+# ]
+
+# # We need to use slice(None) if the user selects 'All'.
+# # The specified level with 'All' will take all values in that level.
+# for idx, level in enumerate(levels):
+#     if level == 'All':
+#         levels[idx] = slice(None)
+
+# # Make a cross section with the level values and pass in the index names.
+# st.dataframe(
+#     df.xs(
+#         (levels[0], levels[1]),
+#         level=['city', 'store']
+#     )
+# )
+
+
+# First, let's set up the multi index with 2 levels:
+# city and store. We will create an empty frame with some
+# column data (fruits) and pass in the multi index as index.
+# Index names are set for accessing later
 multi_index = pd.MultiIndex.from_product([
     ['city_1', 'city_2'],
     ['store_1', 'store_2', 'store_3'],
 ])
-
 df = pd.DataFrame(columns=['apples', 'oranges'], index=multi_index)
 df.index.set_names(['city', 'store'], inplace=True)
-
 # Now let's make some selectboxes for drilling up/down
 levels = [
     st.selectbox('Level 1', ['All'] +
@@ -37,13 +70,11 @@ levels = [
     st.selectbox('Level 2', ['All'] +
                  [i for i in df.index.get_level_values(1).unique()])
 ]
-
 # We need to use slice(None) if the user selects 'All'.
 # The specified level with 'All' will take all values in that level.
 for idx, level in enumerate(levels):
     if level == 'All':
         levels[idx] = slice(None)
-
 # Make a cross section with the level values and pass in the index names.
 st.dataframe(
     df.xs(
